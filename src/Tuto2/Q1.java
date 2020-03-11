17fa483c1c8779f06c370a9c20c6b6621a02005f
HeLin <helinchooi@gmail.com>
2020-03-11 15:06:13 +0800

    Update: use instanceof operator instead

--- src/Tuto2/Q1.java
+++ src/Tuto2/Q1.java
@@ -1,34 +1,32 @@
 package Tuto2;
+
 public class Q1 {
-    //use instanceof to differentiate them. My method is a bit odd haha
-    public static <T> void compareOrSum(T e1,T e2){
-        System.out.printf("The paremeters value are : %s and %s\n",e1,e2);
-        //use instanceof method if generic class is requested
-        String value1 = e1.toString();
-        String[] nums = {"0","1","2","3","4","5","6","7","8","9"};
-        for (int i = 0; i < nums.length; i++) {
-            if(value1.contains(nums[i])){
-                double v1 = Double.parseDouble(value1);
-                double v2 = Double.parseDouble(e2.toString());
-                if(value1.contains(".")){
-                    System.out.printf("The sum of the double is " + (v1+v2) +"\n");
-                }else{
-                    System.out.println("The sum of the integer is " + (int)(v1+v2));
-                }
-                //Ask:can I utilise exception by parsing them into int
-                return;
+
+    public static <T> void compareOrSum(T e1, T e2) {
+        System.out.printf("The paremeters value are : %s and %s\n", e1, e2);
+        if(e1 instanceof String){
+            String value1 = e1.toString();
+            String value2 = e2.toString();
+            if(value1.equals(value2)){
+                System.out.println("The string is equal");
+            }else{
+                System.out.println("The string is not equal");
             }
-        }
-        if(value1.equals(e2.toString())){
-            System.out.println("The string is equal");
-        }else{
-            System.out.println("The string is not equal");
+        }else if(e1 instanceof Double){
+            double value1 = Double.parseDouble(e1.toString());
+            double value2 = Double.parseDouble(e2.toString());
+            System.out.println("The sum of the double are : " + (value1+value2));
+        }else if (e1 instanceof Integer) {
+            int v1 = Integer.parseInt(e1.toString());
+            int v2 = Integer.parseInt(e2.toString());
+            System.out.println("The sum of the integer is " + (v1+v2));
         }
     }
+
     public static void main(String[] args) {
-//        compareOrSum("Hello", "World");
-        compareOrSum(12.4,64.3);
-        compareOrSum(20,35);
-//        compareOrSum("Hello", "Hello");
+        compareOrSum("Hello", "World");
+        compareOrSum(12.4, 64.3);
+        compareOrSum(20, 35);
+        compareOrSum("Hello", "Hello");
     }
 }
