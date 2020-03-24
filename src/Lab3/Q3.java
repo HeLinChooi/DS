87a823c36f2ca6fa1dfe9a47ce7abd96b03b5fd7
HeLinChooi <helinchooi@gmail.com>
2020-03-24 13:27:24 +0800

    modify lab 3

--- src/Lab3/Q3.java
+++ src/Lab3/Q3.java
@@ -13,78 +13,20 @@ public class Q3 {
             System.out.println(str);
         }
     }
-    public static void per(String per,String in){
-        if(in.isEmpty()){
-            checker(per);
-            System.out.println("Suppose is "+per);
+    public static void per(String strToLonger,String original){
+        if(original.isEmpty()){
+            checker(strToLonger);
+            System.out.println("Suppose is "+strToLonger);
         }else{
-            for (int i = 0; i < in.length(); i++) {
-                String Newper = per + in.charAt(i);
-//                System.out.println("per become " + per);
-                String input =  in.substring(0,i) + in.substring(i+1,in.length());
+            for (int i = 0; i < original.length(); i++) {
+                String Newper = strToLonger + original.charAt(i);
+
+                String input =  original.substring(0,i) + original.substring(i+1,original.length());
                 per(Newper,input);
             }
         }
     }
-//     public static void permutationNoDup(String perm, String num) {
-//        if (num.isEmpty()) {
-////            checker(perm + num);
-//        } else {
-//            for (int i = 0; i < num.length(); i++) {
-//                String nextPerm = perm + num.charAt(i);
-//                String nextNum = num.substring(0, i) + num.substring(i + 1, num.length());
-//                permutationNoDup(nextPerm, nextNum);
-//            }
-//        }
-//    }
     public static void main(String[] args) {
         per("101");
     }
-////khai's answer
-//
-//    public static void main(String[] args) {
-////        permutation("436");
-////        System.out.println("");
-////        permutation("5520");
-////        System.out.println("");
-//        permutationNoDup("5520");
-//    }
-//
-//    public static void permutation(String input) {
-//        permutation("", input);
-//    }
-//
-//    public static void permutation(String perm, String num) {
-//        if (num.isEmpty()) {
-//            System.out.println(perm + num);
-//        } else {
-//            for (int i = 0; i < num.length(); i++) {
-//                permutation(perm + num.charAt(i), num.substring(0, i) + num.substring(i + 1, num.length()));
-//            }
-//        }
-//    }
-//    private static String records = " ";
-//    
-//    public static void checker(String str) {
-//        boolean isDup = false;
-//        String[] str2 = records.split(" ");
-//        for (String list : str2) {
-//            System.out.println("This is list:" + list);
-//            if (list.equals(str)) {
-//                isDup = true;
-//                break;
-//            }
-//        }
-//        if (!isDup) {
-//            System.out.println("g" + str);
-//            records += str + " ";
-//        }
-//        System.out.println("End of Checker");
-//    }
-//
-//    public static void permutationNoDup(String input) {
-//        permutationNoDup("", input);
-//    }
-//
-   
 }
