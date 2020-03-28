d2c78cdc7c789bfc4ccf2328b1ae1f79f1cacb8f
HeLinChooi <helinchooi@gmail.com>
2020-03-28 14:50:32 +0800

    made a change

--- src/TestFromSem2DS/X.java
+++ src/TestFromSem2DS/X.java
@@ -1,13 +1,16 @@
 package TestFromSem2DS;
 
-import java.util.LinkedList;
+import java.util.Arrays;
 
 public class X {
-   public static void main(String[] args) {
-       int b = 7;
-       for (int i = 0; i < 10; i++) {
-           b = (b-1)%7;
-           System.out.println(b);
-       }
-   }
+
+    public static void main(String args[]) {
+        String stringArray[] = {"Hello", "how", "are", "you", "welcome", "to", "Tutorialspoint"};
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < stringArray.length; i++) {
+            sb.append(stringArray[i]);
+        }
+        String str = Arrays.toString(stringArray);
+        System.out.println(str);
+    }
 }
